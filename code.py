7993f43d6f163ee652a15c424cd35b2f0e0b309d
Tobias Zucali <tobias@zucali.at>
2024-09-26 13:28:39 +0200

    final

--- code.py
+++ code.py
@@ -36,8 +36,6 @@ async def show_with_progress(message, duration=1, color=None):
 async def hide(message, duration=0):
     await messageboard.animate(message, "Static", "hide", duration=duration)
 
-async def blink(message):
-    await messageboard.animate(message, "Static", "blink")
 
 async def slide_top(message):
     await messageboard.animate(message, "Scroll", "out_to_top")
@@ -45,16 +43,16 @@ async def slide_top(message):
 async def main():
     while True:
         messageboard.set_background(0x000000)
+        message = Message(font_bold_24, mask_color=0xFF00FF, opacity=0.5)
+        message.add_image("images/kunstverein_b.bmp", x_offset=0, y_offset=0)
+        message.add_image("images/artmagazin_b.bmp", x_offset=16, y_offset=0)
+        await reveal(message)
+        await show(message, duration=2)
+        await messageboard.animate(message, "Static", "blink", count=9, duration=2)
         message = Message(font_bold_24, mask_color=0xFF00FF, opacity=0.35)
-        message.add_image("images/artmagazin_b.bmp", x_offset=0, y_offset=0)
-        # await show(message, duration=2)
-        # message = Message(font_bold_24, mask_color=0xFF00FF, opacity=0.5)
-        # message.add_image("images/kunstverein_b.bmp", x_offset=0, y_offset=0)
-        # await show(message, duration=2)
-        # message = Message(font_bold_24, mask_color=0xFF00FF, opacity=0.35)
-        # message.add_image("images/open_night_b.bmp", x_offset=0, y_offset=0)
-        # await show(message, duration=2)
-        await blink(message)
+        message.add_image("images/open_night_b.bmp", x_offset=0, y_offset=0)
+        messageboard.set_message_position(0, 0)
+        await messageboard.animate(message, "Static", "blink", count=9, duration=2)
 
 while True:
     try:
